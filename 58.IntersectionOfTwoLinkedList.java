6d2593fe440d436161a71c647a7df9895d2581ff
xieqilu <xieqilu1989@gmail.com>
2014-12-08 23:57:59 -0800

    Create 58.IntersectionOfTwoLinkedList.java

---
+++ 58.IntersectionOfTwoLinkedList.java
@@ -0,0 +1,80 @@
+/**Write a program to find the node at which the intersection of two singly linked lists begins.
+*
+*
+*For example, the following two linked lists:
+*
+*A:          a1 → a2
+*                   ↘
+*                     c1 → c2 → c3
+*                   ↗            
+*B:     b1 → b2 → b3
+*begin to intersect at node c1.
+*
+*Notes:
+*
+*If the two linked lists have no intersection at all, return null.
+*The linked lists must retain their original structure after the function returns.
+*You may assume there are no cycles anywhere in the entire linked structure.
+*Your code should preferably run in O(n) time and use only O(1) memory.
+*/
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    private int getLength(ListNode head)
+    {
+        int l =0;
+        while(head!=null)
+        {
+            l++;
+            head = head.next;
+        }
+        return l;
+    }
+    
+    private ListNode getIntersectionHelper(int d, ListNode headA, ListNode headB)
+    {
+        for(int i=0;i<d;i++)
+        {
+            if(headA == null)
+                return null;
+            headA = headA.next;
+        }
+        
+        while(headA!=null && headB!=null)
+        {
+            if(headA ==headB)
+                return headA;
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return null;
+    }
+    
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lA = getLength(headA);
+        int lB = getLength(headB);
+        if(lA > lB)
+        {
+            int d = lA-lB;
+            return getIntersectionHelper(d, headA, headB);
+        }
+        else
+        {
+            int d = lB-lA;
+            return getIntersectionHelper(d, headB, headA);
+        }
+    }
+}
